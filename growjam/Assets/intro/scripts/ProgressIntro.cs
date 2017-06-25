1f98b77e181f4f81c22119f43fa043e6eac9877e
Alexandros Gouvatsos <alexandros@gouvatsos.com>
2017-06-25 15:22:53 +0200

    Add intro 'cinematic' scene for the text. Link up scenes. Add arrow asset. Add functionality to main menu.

---
+++ growjam/Assets/intro/scripts/ProgressIntro.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ProgressIntro : MonoBehaviour {
+
+    public Text[] TextList;
+    private int currentTextIndex;
+
+    private void Start()
+    {
+        currentTextIndex = 0;
+    }
+
+    public void Progress()
+    {
+        TextList[currentTextIndex].gameObject.SetActive(false);
+        currentTextIndex++;
+        if (currentTextIndex < TextList.Length)
+            TextList[currentTextIndex].gameObject.SetActive(true);
+        else
+            SceneManager.LoadScene("game");
+    }
+
+}
